aa63d490b7e9299911d605851fbe321cc1bb8ae7
ofunc <ofunc@outlook.com>
2020-02-29 16:34:47 +0800

    Add Aggregate functions

--- dt.go
+++ dt.go
@@ -19,6 +19,51 @@ func IsNA(a Value) bool {
 	}
 }
 
+// First returns the first of list l.
+func First(l List) Value {
+	return l.First()
+}
+
+// Last returns the last of list l.
+func Last(l List) Value {
+	return l.Last()
+}
+
+// Count returns the count of list l.
+func Count(l List) Value {
+	return l.Count()
+}
+
+// Sum returns the sum of list l.
+func Sum(l List) Value {
+	return l.Sum()
+}
+
+// Mean returns the mean of list l.
+func Mean(l List) Value {
+	return l.Mean()
+}
+
+// Var returns the var of list l.
+func Var(l List) Value {
+	return l.Var()
+}
+
+// Std returns the std of list l.
+func Std(l List) Value {
+	return l.Std()
+}
+
+// Max returns the max of list l.
+func Max(l List) Value {
+	return l.Max()
+}
+
+// Min returns the min of list l.
+func Min(l List) Value {
+	return l.Min()
+}
+
 func makeKey(t reflect.Type, r Record, keys []string) interface{} {
 	k := reflect.New(t).Elem()
 	for i, key := range keys {
