85cb7a654909dd864921a04a22887dc5d5aad10e
ofunc <ofunc@outlook.com>
2020-02-29 16:01:46 +0800

    Add List.Filter

--- list.go
+++ list.go
@@ -3,6 +3,17 @@ package dt
 // List is the list data structure.
 type List []Value
 
+// Filter filters the list with function f.
+func (a List) Filter(f func(Value) bool) List {
+	var b List
+	for _, v := range a {
+		if f(v) {
+			b = append(b, v)
+		}
+	}
+	return b
+}
+
 // FillNA fills NA value with value.
 func (a List) FillNA(value Value) List {
 	for i, v := range a {
