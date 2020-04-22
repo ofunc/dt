75b9fdc890c8559e21fe22ef50298039c45b5632
ofunc <ofunc@qq.com>
2020-04-22 12:37:15 +0800

    Move agg functions to agg.go file

---
+++ agg.go
@@ -0,0 +1,46 @@
+package dt
+
+// First returns the first of list l.
+func First(l List) Value {
+	return l.First()
+}
+
+// Last returns the last of list l.
+func Last(l List) Value {
+	return l.Last()
+}
+
+// Count returns the count of list l.
+func Count(l List) Value {
+	return l.Count()
+}
+
+// Sum returns the sum of list l.
+func Sum(l List) Value {
+	return l.Sum()
+}
+
+// Mean returns the mean of list l.
+func Mean(l List) Value {
+	return l.Mean()
+}
+
+// Var returns the var of list l.
+func Var(l List) Value {
+	return l.Var()
+}
+
+// Std returns the std of list l.
+func Std(l List) Value {
+	return l.Std()
+}
+
+// Min returns the min of list l.
+func Min(l List) Value {
+	return l.Min()
+}
+
+// Max returns the max of list l.
+func Max(l List) Value {
+	return l.Max()
+}
